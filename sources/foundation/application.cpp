20f0361a426db48240d17d928557aead76457298
E1Hephaestus <gefestroyale@gmail.com>
2021-11-26 10:20:16 +0200

    Initial structure

---
+++ sources/foundation/application.cpp
@@ -0,0 +1,44 @@
+#include "foundation/application.hpp"
+#include <core/print.hpp>
+
+Application::Application(){
+    m_MainWindow.Open(1280, 720, "UtterCraft");
+
+    Function<void(const Event&)> handler;
+    handler.Bind<Application, &Application::OnEvent>(this);
+    m_MainWindow.SetEventsHanlder(handler);
+
+}
+
+int Application::Run(){
+    Clock cl;
+    while(m_MainWindow.IsOpen()){
+        float dt = cl.GetElapsedTime().AsSeconds();
+        cl.Restart();
+        m_MainWindow.DispatchEvents();
+
+        Tick(dt);
+    }
+
+    return 0;
+}
+
+void Application::Tick(float dt){
+
+}
+
+void Application::OnEvent(const Event &e){
+    Println("OnEvent");
+
+    if(e.Type == EventType::WindowClose)
+        Stop();
+}
+
+void Application::Stop(){
+    m_MainWindow.Close();
+}
+
+Application &Application::Get(){
+    static Application app;
+    return app;
+}
---
+++ sources/main.cpp
@@ -0,0 +1,7 @@
+#include <core/print.hpp>
+#include <core/span.hpp>
+#include "foundation/application.hpp"
+
+int StraitXMain(Span<const char *> args){
+    return Application::Get().Run();
+}
